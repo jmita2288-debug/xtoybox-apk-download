4e9a364f3f32ab55ae88f4f3006c99402d44d7b8
jmita2288-debug <jmita2288@gmail.com>
2026-05-16 10:11:35 -0300

    Render report bug page in Vercel entry

--- src/vercel-entry.tsx
+++ src/vercel-entry.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Index } from "./routes/index";
+import { ReportarBugsPage } from "./routes/reportar-bugs";
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
@@ -9,8 +10,18 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
+function App() {
+  const path = window.location.pathname.replace(/\/$/, "") || "/";
+
+  if (path === "/reportar-bugs") {
+    return <ReportarBugsPage />;
+  }
+
+  return <Index />;
+}
+
 createRoot(rootElement).render(
   <React.StrictMode>
-    <Index />
+    <App />
   </React.StrictMode>,
 );
