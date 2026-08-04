08e0623e6fe24cee3724073497e25133072569b0
jmita2288-debug <jmita2288@gmail.com>
2026-08-04 17:59:37 -0300

    Ativa carrossel de fundo na página inicial

--- src/vercel-entry.tsx
+++ src/vercel-entry.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { HeroBackgroundCarousel } from "./components/HeroBackgroundCarousel";
 import { Index } from "./routes/index";
 import { ReportarBugsPage } from "./routes/reportar-bugs";
 import "./styles.css";
+import "./hero-carousel.css";
 
 const rootElement = document.getElementById("root");
 
@@ -18,7 +20,12 @@ function App() {
     return <ReportarBugsPage />;
   }
 
-  return <Index />;
+  return (
+    <>
+      <Index />
+      <HeroBackgroundCarousel />
+    </>
+  );
 }
 
 createRoot(rootElement).render(
