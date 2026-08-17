367519c711749e628aad959cbe2783d37790def3
Alex69 <jmita2288@gmail.com>
2026-08-17 03:17:54 -0300

    Ativa gateway opcional de anúncio no download

--- src/vercel-entry.tsx
+++ src/vercel-entry.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { DownloadAdGate } from "./components/DownloadAdGate";
 import { DownloadCounter } from "./components/DownloadCounter";
 import { SocialContactEnhancer } from "./components/SocialContactEnhancer";
 import { RefinedIndex } from "./routes/index-refined";
@@ -8,6 +9,7 @@ import { ReportarBugsPage } from "./routes/reportar-bugs";
 import "./styles.css";
 import "./refined-site.css";
 import "./download-counter.css";
+import "./download-ad-gate.css";
 import "./social-contact.css";
 
 const rootElement = document.getElementById("root");
@@ -34,6 +36,7 @@ function App() {
 createRoot(rootElement).render(
   <React.StrictMode>
     <App />
+    <DownloadAdGate />
     <SocialContactEnhancer />
     <SpeedInsights />
   </React.StrictMode>,
